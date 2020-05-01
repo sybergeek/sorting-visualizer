be677f610f34c908d2907492a821a2655c3f43cc
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-05-01 20:04:26 +0530

    Random array bars generated

---
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './SortingVisualizer.css'
+
+class SortingVisualizer extends React.Component {
+	state = {
+		array: [],
+	}
+
+	componentDidMount() {
+		this.resetArray()
+	}
+
+	resetArray() {
+		const array = [];
+		for (let i = 0; i < 200; ++i) {
+			array.push(randomIntFromInterval(5, 500));
+		}
+		this.setState({array})
+	}
+
+	render() {
+		const {array} = this.state
+
+		return (
+			<div className="array-container">
+				{array.map((value, index) => (
+					<div
+						className="array-bar"
+						key={index}
+						style={{
+							backgroundColor: 'red',
+							height: `${value}px`,
+						}}
+					></div>
+				))}
+				<button onClick={() => this.resetArray()}>Generate New Array</button>
+			</div>
+		)
+	}
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default SortingVisualizer
